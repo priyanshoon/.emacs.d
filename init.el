f6104e4491c783e6a72092df5211f32849d6c3bc
rootpalladium <guptapriyanshu973@gmail.com>
2023-07-24 12:15:35 +0530

    themes and vi

--- init.el
+++ init.el
@@ -10,9 +10,8 @@
 (setq visible-bell t)
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
 
-(load-theme 'wombat)
-
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(set-frame-parameter nil 'alpha-background 70)
 
 (require 'package)
 
@@ -32,6 +31,13 @@
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
+
+(use-package ef-themes
+  :if window-system
+  :ensure t
+  :config
+  ;; Enable the theme
+  (load-theme 'ef-symbiosis t))
 
 (dolist (mode '(org-mode-hook
 		term-mode-hook
@@ -77,3 +83,61 @@
 	 ("C-r" . 'counsel-minibuffer-history))
   :config
   (setq ivy-initial-inputs-alist nil))
+
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+  (rune/leader-keys
+   "t" '(:ignore t :which-key "toggles")
+   "tt" '(counsel-load-theme :which-key "choose theme")))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(chart-face-color-list
+   '("#b52c2c" "#0fed00" "#f1e00a" "#2fafef" "#bf94fe" "#47dfea" "#702020" "#007800" "#b08940" "#1f2f8f" "#5f509f" "#00808f"))
+ '(custom-safe-themes
+   '("e5616f027ca0c17597ae35e6643a129b4ddaf0f64fdf45669b561e0e47c3ada5" default))
+ '(flymake-error-bitmap '(flymake-double-exclamation-mark ef-themes-mark-delete))
+ '(flymake-note-bitmap '(exclamation-mark ef-themes-mark-select))
+ '(flymake-warning-bitmap '(exclamation-mark ef-themes-mark-other))
+ '(global-hl-line-mode nil)
+ '(ibuffer-deletion-face 'ef-themes-mark-delete)
+ '(ibuffer-filter-group-name-face 'bold)
+ '(ibuffer-marked-face 'ef-themes-mark-select)
+ '(ibuffer-title-face 'default)
+ '(package-selected-packages '(ef-themes general which-key use-package ivy-rich counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
